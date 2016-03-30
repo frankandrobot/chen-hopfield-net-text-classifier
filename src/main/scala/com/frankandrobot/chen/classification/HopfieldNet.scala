92d7d93aa7b128881f3c2a0a7a95738f26a36a21
Uriel Avalos <uriel.avalos@drillinginfo.com>
2016-03-29 19:55:51 -0500

    2nd pass hopfield

--- src/main/scala/com/frankandrobot/chen/classification/HopfieldNet.scala
+++ src/main/scala/com/frankandrobot/chen/classification/HopfieldNet.scala
@@ -1,5 +1,6 @@
 package com.frankandrobot.chen.classification
 
+import com.frankandrobot.chen.DocTypes.Term
 import com.frankandrobot.chen.cluster.ClusterWeights
 import com.frankandrobot.chen.docs.TermStore
 import com.frankandrobot.chen.indexer.Indexer
@@ -8,7 +9,7 @@ import scala.math.exp
 
 class HopfieldNet(termStore: TermStore, clusterWeights: ClusterWeights, indexer: Indexer) {
 
-  lazy val n = termStore.terms.length
+  private def n() = termStore.terms.length
 
   def relatedConcepts(theta_j : Double, theta_o : Double, epsilon : Double)(word : String) = {
 
@@ -32,26 +33,37 @@ class HopfieldNet(termStore: TermStore, clusterWeights: ClusterWeights, indexer:
 
             var t = 0
             while(error(t) > epsilon) { t += 1 }
+
+            (0 to n - 1).foldLeft(List[Term]()){ (total, i) => {
+
+              _mu(i, t + 1) match {
+                case m if m > 0 => termStore.terms()(i) :: total
+                case _ => total
+              }
+            }}
           }
         }
       }
     }
   }
 
-  def _lookup(term : String) = termStore.termMap.get(term)
+  private def _lookup(term : String) = termStore.termMap.get(term)
 
-  def mu(theta_j : Double, theta_o : Double, inputIndex : Int)(i : Int, t : Int) : Double = {
+  private def mu(theta_j : Double, theta_o : Double, inputIndex : Int)(j : Int, t : Int) : Double = {
 
-    val _mu = mu(theta_j, theta_o, inputIndex)
-    val _fs = fs(theta_j, theta_o)
+    t match {
+      case 0 => if (j == inputIndex) {1.0} else {0.0}
+      case t if t > 0 => {
 
-    val sum = t match {
-      case 0 => if (i == inputIndex) {1.0} else {0.0}
-      case t if t > 0 => (0 to n - 1).foldLeft(0.0)((total, j) => total + clusterWeights.weights(j, i) * _mu(j, t - 1))
+        val _mu = mu(theta_j, theta_o, inputIndex)
+        val _fs = fs(theta_j, theta_o)
+
+        val sum = (0 to n - 1).foldLeft(0.0)((total, i) => total + clusterWeights.weights(i, j) * _mu(i, t - 1))
+
+        _fs(sum)
+      }
     }
-
-    _fs(sum)
   }
 
-  def fs(theta_j : Double, theta_o : Double)(net : Double) = 1.0 / (1.0 + exp((-(net - theta_j) / theta_o)))
+  private def fs(theta_j : Double, theta_o : Double)(net : Double) = 1.0 / (1.0 + exp((-(net - theta_j) / theta_o)))
 }
