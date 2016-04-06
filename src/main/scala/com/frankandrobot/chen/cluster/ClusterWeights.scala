59c6dfe3c66f5db1cb7e16f54c3b7e71ef9cdfc8
Uriel Avalos <uriel.avalos@drillinginfo.com>
2016-04-06 17:34:40 -0500

    [refac]

--- src/main/scala/com/frankandrobot/chen/cluster/ClusterWeights.scala
+++ src/main/scala/com/frankandrobot/chen/cluster/ClusterWeights.scala
@@ -10,19 +10,19 @@ class ClusterWeights(termStore: TermStore,
                      clusterAnalysis: ClusterAnalysis,
                      rawTermsByDocStore: DocStore) {
 
-  def weight(docWithRawTerms : Doc, termIndex : Int) = {
+  def weight(doc : Doc, termIndex : Int) = {
 
     val term = termStore.terms()(termIndex)
 
-    clusterAnalysis.termFrequency(docWithRawTerms, term) * log10(clusterAnalysis.docFrequency(term))
+    clusterAnalysis.termFrequency(doc, term) * log10(clusterAnalysis.docFrequency(term))
   }
 
-  def weight(docWithRawTerms: Doc, termIndex1 : Int, termIndex2 : Int) = {
+  def weight(doc: Doc, termIndex1 : Int, termIndex2 : Int) = {
 
     val term1 = termStore.terms()(termIndex1)
     val term2 = termStore.terms()(termIndex2)
 
-    clusterAnalysis.termFrequency(docWithRawTerms, term1, term2) * log10(clusterAnalysis.docFrequency(term1, term2))
+    clusterAnalysis.termFrequency(doc, term1, term2) * log10(clusterAnalysis.docFrequency(term1, term2))
   }
 
 }
